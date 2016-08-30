70eac273aa73a3dd0116d62c5a7e92ed22d6e396
Jason L. Bogle <kicker10bog@gmail.com>
2016-08-30 00:29:27 -0500

    fixing repo

--- adminBot.php
+++ adminBot.php
@@ -1,5 +1,17 @@
-<?php
-// the main bot setupinclude('./httpful.phar');include('./functions.php');
-// this is found at http://dev.groupme.com/bots$bot_token = "bc0386edbcfbd6522525e3644f";// adds admin commands $isAdmin = TRUE;include('adminFunctions.php');
-include('./index.php');
+<?php
+
+// the admin bot setup
+
+include('./httpful.phar');
+include('./functions.php');
+
+// this is found at http://dev.groupme.com/bots
+$bot_token = "admin bot id";
+
+// adds admin commands 
+$isAdmin = TRUE;
+include('adminFunctions.php');
+
+include('./index.php');
+
 ?>
